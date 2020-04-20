0dba3303fbe4ff0f04df453e35b47370c87e8a72
irao22 <irao22@deerfield.edu>
2020-04-20 13:52:25 -0400

    Working World

--- Dinosaur.java
+++ Dinosaur.java
@@ -12,33 +12,39 @@ public class Dinosaur extends Animal {
 
 	}
 	@Override
-	public void reproduce() {
-		int newX = (int)(Math.random()*20);
-		int newY = (int)(Math.random()*20); 
-		myWorld.getCreatureList().add(new Cow(new Location(newX,newY), myWorld));
-	}
-	
-	public void move(int i) {
-		int myX = myWorld.getCreatureList().get(i).getMyLocation().getX();
-		int myY = myWorld.getCreatureList().get(i).getMyLocation().getY();
-		Location loc = new Location(myX+1, myY+1);
-		myWorld.getCreatureList().get(i).setMyLocation(loc);
-	}
-	
-	public void eatCow() {
+	public void reproduce(int index) {
+		System.out.print("Reproduce");
+		int x= myWorld.getCreatureList().get(index).getMyLocation().getX();
+		int y= myWorld.getCreatureList().get(index).getMyLocation().getY();
+		Location updated = new Location (x,y);
+		Location updated2 = new Location(x+1,y);
+		Location updated3 = new Location (x-1,y);
+		Location updated4= new Location (x, y+1);
+		Location updated5 = new Location (x, y-1);
+		Location updated6 = new Location (x+1, y+1);
+		Location updated7 = new Location (x-1, y-1);
 		for (int i=0; i<myWorld.getCreatureList().size(); i++) {
-			if (myLocation == myWorld.getCreatureList().get(i).getMyLocation()) {
-				
+			if ((myWorld.getCreatureList().get(i).getMyLocation().getX()==updated.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated2.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated2.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated3.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated3.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated4.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated4.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated5.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated5.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated6.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated6.getY() || (myWorld.getCreatureList().get(i).getMyLocation().getX()==updated7.getX()) && (myWorld.getCreatureList().get(i).getMyLocation().getY())==updated7.getY()) {
+				if (myWorld.getCreatureList().get(i).getSpecies().compareTo(myWorld.getCreatureList().get(index).getSpecies())==0) {
+							if(myWorld.getCreatureList().get(i).getSex()!=myWorld.getCreatureList().get(index).getSex()) {
+								Location offspring = new Location(updated.getX()+rgen.nextInt(-1, 1), updated.getY()+rgen.nextInt(-1, 1));
+									myWorld.getCreatureList().add(new Dinosaur(offspring, myWorld));
+				}
+					}
 			}
-			
+			}
 		}
-	}
+		
+	
+	
+	
+	
 	
 	public void eat() {
 		for (int i=0; i<myWorld.getCreatureList().size(); i++) {
 			if (myLocation.getX()==myWorld.getCreatureList().get(i).getMyLocation().getX()&&myLocation.getY()==myWorld.getCreatureList().get(i).getMyLocation().getY()) {
 				String species2=myWorld.getCreatureList().get(i).getSpecies();
-				if(species2.compareTo("Poison Berries")!=0) {
+				if(species2.compareTo("Poison Berries")!=0&&species2.compareTo("Dinosaur")!=0) {
 					myWorld.getCreatureList().get(i).setAge(500);
 				}else if(species2.compareTo("Poison Berries")==0) {
 					myAge=500;
@@ -46,9 +52,6 @@ public class Dinosaur extends Animal {
 		
 			}
 			
-		}
-	}
-	
-	
+		}}
 
 }
---
+++ WorldController.java
@@ -0,0 +1,103 @@
+import java.awt.Color;
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+public class WorldController extends GraphicsProgram {
+	
+	private World theWorld;
+	private GCanvas theWorldCanvas;
+	public static final int APPLICATION_WIDTH = 1000;
+	public static final int APPLICATION_HEIGHT = 1000;
+	
+	public void run(){	
+		setUpWorld();
+		runWorld();
+		System.out.println("Finished running main");
+	}
+	
+	public void init(){
+	    resize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+	}
+	
+	public void setUpWorld() {
+		theWorld= new World (20,20);
+		theWorld.getCreatureList().add(new Grass(new Location(3,6), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(4,6), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(11,6), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(12,6), theWorld ));
+		theWorld.getCreatureList().add(new Grass(new Location(10,5), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(1,13), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(8,10), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(1,13), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(12,5), theWorld ));
+		theWorld.getCreatureList().add(new Grass( new Location(7,8), theWorld ));
+		theWorld.getCreatureList().add(new Cow( new Location(2,2), theWorld ));
+		theWorld.getCreatureList().add(new Cow( new Location(3,2), theWorld ));
+		theWorld.getCreatureList().add(new Cow( new Location(2,5), theWorld ));
+		theWorld.getCreatureList().add(new Dinosaur(new Location (3,12), theWorld));
+		theWorld.getCreatureList().add(new Dinosaur(new Location (9,6), theWorld));
+		theWorld.getCreatureList().add(new Dinosaur(new Location (2,4), theWorld));
+		theWorld.getCreatureList().add(new Dinosaur(new Location (2,13), theWorld));
+		theWorld.getCreatureList().add(new Dinosaur(new Location (8,7), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (6,4), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (6,5), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (3,9), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (8,4), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (7,5), theWorld));
+		theWorld.getCreatureList().add(new PoisonBerries(new Location (10,9), theWorld));
+		theWorld.getCreatureList().add(new Mushroom(new Location (1,5), theWorld));
+		theWorld.getCreatureList().add(new Mushroom(new Location (4,9), theWorld));
+		theWorld.getCreatureList().add(new Finch(new Location (3,2), theWorld));
+		theWorld.getCreatureList().add(new Finch(new Location (3,4), theWorld));
+		theWorld.getCreatureList().add(new Finch(new Location (12,6), theWorld));
+		theWorld.getCreatureList().add(new Finch(new Location (11,7), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (1,1), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (0,2), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (7,10), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (0,15), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (1,13), theWorld));
+		theWorld.getCreatureList().add(new Human(new Location (2,12), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (1,3), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (10,8), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (10,9), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (11,3), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (4,8), theWorld));
+		theWorld.getCreatureList().add(new Flower(new Location (6,9), theWorld));
+		theWorldCanvas = this.getGCanvas();
+	}
+	
+	public void runWorld(){
+		drawWorld();
+		for(int i=0; i<50;i++){
+			theWorld.letTimePass();
+			pause(500);
+			drawWorld();
+		}
+	}	
+	
+	public void drawWorld(){
+		drawBlankWorld();
+		drawCreatures();
+	}
+	
+	public void drawBlankWorld(){
+		for(int row = 0 ; row<theWorld.getWidth(); row++)
+			for(int col=0; col<theWorld.getHeight(); col++){
+				GRect r = new GRect(row*50, col*50, 50, 50);
+				r.setFillColor(Color.WHITE);
+				r.setFilled(true);
+				theWorldCanvas.add(r);
+			}
+	}
+	
+	public void drawCreatures(){
+		for(LifeForm x: theWorld.getCreatureList()){
+			GRect r = new GRect (x.getMyLocation().getX()*50, x.getMyLocation().getY()*50,50,50);
+			r.setFillColor(x.getMyColor());
+			r.setFilled(true);
+			theWorldCanvas.add(r);
+		}
+	}
+}
